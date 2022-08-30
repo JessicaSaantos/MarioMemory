a0b43d74305ced4ac5c0c696105cd2b30339951b
JessicaSaantos <jessicaferre998@gmail.com>
2022-08-30 17:27:45 -0300

    desenvolvendo JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 const main = document.getElementById('main')
-renderizarCartas()
+const placarTag = document.getElementById("placar");
+const buttonEncerrar = document.getElementById("encerrar");
+let click01 = null;
+let click02 = null;
+let encontrados = 0;
+let placar = 0;
+
+renderizarCartas();
+// ==================== Preencher o layout de cartas =========
 function renderizarCartas(){
 
     const arrDuplicado = [...personagens, ...personagens]
@@ -17,6 +25,8 @@ function renderizarCartas(){
         main.appendChild(carta)
     }
 }
+
+// ==================== Identificando click nas cartas =========
 
 function clickCarta (event){
     const imgClicada = event.target
@@ -36,9 +46,37 @@ function clickCarta (event){
 
 }
 
+// ==================== Verificando se é Par correto e alterando placar =========
+
 function testePar (){
-    if (click01.id == click02.id)
+    if (click01.id == click02.id){
+        encontrados++
+        click01 = null
+        click02 = null
+
+        if (encontrados == 4)
+        {
+            placar++
+            placarTag.innerHTML = placar
+            encontrados = 0
+
+            setTimeout(() => {
+                main.innerHTML = ""
+                renderizarCartas()
+            }, 2000)
+        }
+        else {
+            setTimeout(() => {
+                click01.src = "./assets/img/QuestionBlock.png"
+                click02.src = "./assets/img/QuestionBlock.png"
+                click01 = null
+                click02 = null
+            }, 1000)
+        }
+    }
 }
+
+// ==================== Criando ordem aleatória para embaralhar array =========
 
 function shuffleArray(arr){
     for (let i = arr.length - 1; i > 0; i --){
