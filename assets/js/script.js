88aa3040d60913434cc4fc094797d8cb1733c7df
JessicaSaantos <jessicaferre998@gmail.com>
2022-08-29 23:59:05 -0300

    iniciando JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@ const main = document.getElementById('main')
 renderizarCartas()
 function renderizarCartas(){
 
-    for(let i = 0; i < 8; i ++){
+    for(let i = 0; i < personagens.length; i ++){
         let carta = document.createElement("img")
         carta.src = "./assets/img/QuestionBlock.png"
         main.appendChild(carta)
     }
 }
+
+console.log(personagens)
