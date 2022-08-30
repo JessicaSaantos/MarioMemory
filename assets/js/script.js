09956255af9f78fdcfcf47641b7ed5deb195aa4d
JessicaSaantos <jessicaferre998@gmail.com>
2022-08-30 20:05:03 -0300

    desenvolvendo + JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,6 @@ function renderizarCartas(){
     const arrDuplicado = [...personagens, ...personagens]
     const embaralhados = shuffleArray(arrDuplicado)
     
-
     for(let i = 0; i < embaralhados.length; i ++){
         let carta = document.createElement("img")
         
@@ -29,13 +28,14 @@ function renderizarCartas(){
 // ==================== Identificando click nas cartas =========
 
 function clickCarta (event){
-    const imgClicada = event.target
+    let imgClicada = event.target
     let idClicado = event.target.id
 
     let personagemClicado = personagens.find(
         (elemento) => elemento.id == idClicado
     )
-    imgClicada.src = personagemClicado.img
+    
+    imgClicada.src = personagemClicado.img;
 
     if (click01 == null){
         click01 = imgClicada
@@ -43,7 +43,6 @@ function clickCarta (event){
         click02 = imgClicada
         testePar()
     }
-
 }
 
 // ==================== Verificando se é Par correto e alterando placar =========
@@ -51,28 +50,28 @@ function clickCarta (event){
 function testePar (){
     if (click01.id == click02.id){
         encontrados++
-        click01 = null
-        click02 = null
+        click01 = null;
+        click02 = null;
 
         if (encontrados == 4)
         {
-            placar++
-            placarTag.innerHTML = placar
-            encontrados = 0
+            placar++;
+            placarTag.innerHTML = placar;
+            encontrados = 0;
 
             setTimeout(() => {
-                main.innerHTML = ""
-                renderizarCartas()
-            }, 2000)
+            main.innerHTML = "";
+            renderizarCartas();
+            }, 2000);
         }
-        else {
-            setTimeout(() => {
-                click01.src = "./assets/img/QuestionBlock.png"
-                click02.src = "./assets/img/QuestionBlock.png"
-                click01 = null
-                click02 = null
-            }, 1000)
-        }
+    }
+    else {
+        setTimeout(() => {
+            click01.src = "./assets/img/QuestionBlock.png";
+            click02.src = "./assets/img/QuestionBlock.png";
+            click01 = null;
+            click02 = null;
+          }, 1000);
     }
 }
 
