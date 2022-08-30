69d1aaaf04382ee16d1c4d8c9bda0311a9b0d546
JessicaSaantos <jessicaferre998@gmail.com>
2022-08-30 15:02:19 -0300

    adicionando funcionalidade de embralhar o array cartas

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,31 @@ const main = document.getElementById('main')
 renderizarCartas()
 function renderizarCartas(){
 
-    for(let i = 0; i < personagens.length; i ++){
+    const arrDuplicado = [...personagens, ...personagens]
+    const embaralhados = shuffleArray(arrDuplicado)
+    console.log(arrDuplicado)
+
+    for(let i = 0; i < embaralhados.length; i ++){
         let carta = document.createElement("img")
+        
+        carta.id = embaralhados[i].id
+        carta.addEventListener("click", clickCarta)
+
         carta.src = "./assets/img/QuestionBlock.png"
+
         main.appendChild(carta)
     }
 }
 
-console.log(personagens)
+function clickCarta (){
+
+}
+function shuffleArray(arr){
+    for (let i = arr.length - 1; i > 0; i --){
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [[arr[j]], arr[i]];
+    }
+    return arr;
+}
+
+
